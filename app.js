3e0ba486fc8a73496f67a8000b5d8e52086b3a82
Cole Koerner <colekoerner.ck@gmail.com>
2023-09-12 16:58:14 -0500

    finishJWT

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+const cookieParser = require('cookie-parser');
+const cron = require('node-cron')
+
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
+
+// cookies
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -17,7 +25,28 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
-
+app.get('*', checkUser);
+app.get('/', requireAuth, (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true');
+
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('you got the cookies!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+
+});
